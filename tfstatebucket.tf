31791154a9c12defda70b3ae98efbbe3fcce67ec
n cha <n.cha@example.com>
2023-11-18 21:50:13 +0200

    remove dublicated provider

--- tfstatebucket.tf
+++ tfstatebucket.tf
@@ -1,8 +1,3 @@
-provider "google" {
-  project = var.gcp_project
-  region  = var.region
-}
-
 resource "google_storage_bucket" "terraform_state" {
   name          = var.tfstate_bucket
   location      = var.region
