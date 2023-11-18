2f93cd0828cebee82578d42d45265217ad41b060
n cha <n.cha@example.com>
2023-11-19 00:43:15 +0200

    cleanup

--- wp-db.tf
+++ wp-db.tf
@@ -1,16 +1,3 @@
-resource "google_sql_database_instance" "wordpress_db" {
-  name             = var.cloud_sql_name
-  database_version = "MYSQL_5_7"
-  region           = var.region
-
-  settings {
-    tier = "db-f1-micro"
-    ip_configuration {
-      private_network = google_compute_network.default.self_link
-    }
-  }
-}
-
 resource "google_sql_database" "wordpress" {
   name     = "wordpress"
   instance = google_sql_database_instance.wordpress_db.name
