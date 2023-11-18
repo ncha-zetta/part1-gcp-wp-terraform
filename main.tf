82bbd89172c8a167d93ac312da2931dfc0557c33
n cha <n.cha@example.com>
2023-11-18 23:39:50 +0200

    clenup of main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,3 @@
-provider "google" {
-  project = var.gcp_project
-  region  = var.region
-}
-
 resource "google_container_cluster" "gke_cluster" {
   name     = "wordpress-cluster"
   location = var.region
