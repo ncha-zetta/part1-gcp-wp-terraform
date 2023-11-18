ea15447a696a53a4f4cebe48bd96d5f3c7ae2af1
n cha <n.cha@example.com>
2023-11-18 23:42:22 +0200

    add load balancer for wp

---
+++ wp-lb.tf
@@ -0,0 +1,18 @@
+resource "kubernetes_service" "wordpress" {
+  metadata {
+    name = "wordpress"
+  }
+
+  spec {
+    selector = {
+      app = "wordpress"
+    }
+
+    port {
+      port        = 80
+      target_port = 80
+    }
+
+    type = "LoadBalancer"
+  }
+}
