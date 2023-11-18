72f46b9d61153f57b6736c8320b2c49291a6a3d0
n cha <n.cha@example.com>
2023-11-18 23:42:06 +0200

    make wp deployment via terraform

---
+++ wp-gke-deployment.tf
@@ -0,0 +1,54 @@
+resource "kubernetes_deployment" "wordpress" {
+  metadata {
+    name = "wordpress"
+  }
+
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        app = "wordpress"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "wordpress"
+        }
+      }
+
+      spec {
+        container {
+          image = "wordpress:latest"
+          name  = "wordpress"
+
+          env {
+            name  = "WORDPRESS_DB_HOST"
+            value = google_sql_database_instance.wordpress_db.public_ip_address
+          }
+
+          env {
+            name  = "WORDPRESS_DB_USER"
+            value = "wp_user"
+          }
+
+          env {
+            name  = "WORDPRESS_DB_PASSWORD"
+            value = var.db_password
+          }
+
+          env {
+            name  = "WORDPRESS_DB_NAME"
+            value = "wordpress"
+          }
+
+          ports {
+            container_port = 80
+          }
+          
+        }
+      }
+    }
+  }
+}
